92f8bbc1e1cd439f5770b4a9f252fdd622f2be65
ptkwork <peter.kinder@wales.nhs.uk>
2024-09-03 16:01:55 +0100

    removed embedded MS Form to redirect due to Chrome bug

--- scripts.js
+++ scripts.js
@@ -7,5 +7,22 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const openButton = document.getElementById('open-form');
+    const modal = document.getElementById('form-modal');
+    const closeButton = document.querySelector('.close');
 
+    openButton.onclick = function() {
+        modal.style.display = 'block';
+    }
 
+    closeButton.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
